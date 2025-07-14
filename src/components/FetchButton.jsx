0495cb73508088e521f447237bf6e41a2646ba63
ndurushima <ndu1994@gmail.com>
2025-07-14 14:09:45 -0500

    final draft of jokes lab.

--- src/components/FetchButton.jsx
+++ src/components/FetchButton.jsx
@@ -1,9 +1,11 @@
 // Step 1: Accept `fetchJoke` as a prop
+import React, {useState, useEffect} from "react"
+
 
 const FetchButton = ({ fetchJoke }) => {
   return (
     // Step 2: Add an onClick event that calls `fetchJoke`
-    <button className="fetch-button">Get a New Joke</button>
+    <button className="fetch-button" onClick={fetchJoke}>Get a New Joke</button>
   )
 }
 
--- src/components/JokeDisplay.jsx
+++ src/components/JokeDisplay.jsx
@@ -1,10 +1,17 @@
 // Step 1: Accept `joke` and `loading` as props
+import React, {useState, useEffect} from "react";
 
 const JokeDisplay = ({ joke, loading }) => {
   return (
     <div className="joke-container">
       {/* Step 2: If `loading` is true, display "Loading..." */}
       {/* Step 3: Otherwise, display the joke */}
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{joke}</p>
+      )}
     </div>
   )
 }
